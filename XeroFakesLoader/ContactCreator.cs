09f6d7e78b81a4b092cc9aae066d381a1f1b0000
Mikhail Belov <mpbelov@gmail.com>
2016-12-08 11:17:01 +0300

    Add project files.

---
+++ XeroFakesLoader/ContactCreator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xero.Api.Core.Model;
+using Xero.Api.Core.Model.Types;
+
+namespace XeroFakesLoader {
+    public class ContactCreator {
+        static string[] currencies = new string[] {
+            "USD", "EUR", "GBP", "INR", "AUD", "CAD", "SGD", "CHF", "MYR", "JPY", "CNY"
+        };
+        Random random = new Random();
+
+        public Contact GenerateRandomContact() {
+            var contact = new Contact();
+            contact.Name = Faker.Company.Name();
+            if (random.Next(5) == 0) {
+                contact.FirstName = Faker.Name.First();
+                contact.LastName = Faker.Name.Last();
+                contact.EmailAddress = Faker.Internet.Email(contact.FirstName);
+                contact.ContactPersons = new List<ContactPerson>();
+
+                for (int i = 0; i < random.Next(6); i++) {
+                    var newPerson = new ContactPerson();
+                    newPerson.FirstName = Faker.Name.First();
+                    newPerson.LastName = Faker.Name.Last();
+                    newPerson.EmailAddress = Faker.Internet.Email(newPerson.FirstName);
+                    newPerson.IncludeInEmails = random.Next(2) == 0;
+                    contact.ContactPersons.Add(newPerson);
+                }
+            }
+            contact.Addresses = new List<Address>() {
+                new Address() {
+                    AddressType = AddressType.PostOfficeBox,
+                    City = Faker.Address.City(),
+                    AddressLine1 = Faker.Address.StreetAddress(),
+                    Country = Faker.Address.Country(),
+                    PostalCode = Faker.Address.ZipCode(),
+                    AttentionTo = Faker.Lorem.Sentence()
+                },
+            };
+            for (int i = 0; i < random.Next(4); i++) {
+                contact.Addresses.Add(
+                    new Address() {
+                        AddressType = AddressType.Street,
+                        City = Faker.Address.City(),
+                        AddressLine1 = Faker.Address.StreetAddress(),
+                        Country = Faker.Address.Country(),
+                        PostalCode = Faker.Address.ZipCode(),
+                        AttentionTo = Faker.Lorem.Sentence()
+                    }
+                );
+            }
+            contact.DefaultCurrency = currencies[random.Next(currencies.Length)];
+            contact.Phones = new List<Phone>();
+            for (int i = 0; i < random.Next(3); i++) {
+                var newPhone = new Phone();
+                newPhone.PhoneType = (PhoneType)i;
+                newPhone.PhoneNumber = Faker.Phone.Number();
+            }
+            return contact;
+        }
+    }
+}
---
+++ XeroFakesLoader/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xero.Api.Core;
+using Xero.Api.Core.Model;
+using Xero.Api.Core.Model.Types;
+using Xero.Api.Example.Applications.Private;
+using Xero.Api.Example.Applications.Public;
+using Xero.Api.Example.TokenStores;
+using Xero.Api.Infrastructure.OAuth;
+using Xero.Api.Serialization;
+using XeroFakesLoader.Properties;
+
+namespace XeroFakesLoader {
+    class Program {
+        static void Main(string[] args) {
+            var apiCore = new XeroCoreApi(
+                "https://api.xero.com/api.xro/2.0/",
+                new PrivateAuthenticator(getCert()),
+                new Consumer("&CHANGE_TO_ACTUAL_CONSUMER_KEY&", "%CHANGE_TO_ACTUAL_CONSUMER_SECRET%"),  // https://app.xero.com/Application/Edit/...
+                null,
+                new DefaultMapper(),
+                new DefaultMapper());
+
+
+            var stopwatch = Stopwatch.StartNew();
+            Console.WriteLine("Generating contacts");
+            var contactCreator = new ContactCreator();
+            var contacts = new List<Contact>();
+            for (int i = 0; i < 500; i++) {
+                var newContact = contactCreator.GenerateRandomContact();
+                contacts.Add(newContact);
+            }
+            Console.WriteLine($"Generation contacts completed and took {stopwatch.Elapsed}");
+            Console.WriteLine("Uploading contacts into Xero company");
+            stopwatch = Stopwatch.StartNew();
+            try {
+                apiCore.Contacts.Create(contacts);
+                Console.WriteLine($"Uploading contacts into Xero company completed and took {stopwatch.Elapsed}");
+            }
+            catch (Exception ex) {
+                Console.Write($"Exception occured while uploading contacts {ex.ToString()}");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static X509Certificate2 getCert() {
+            X509Certificate2 cert = new X509Certificate2();
+            cert.Import(Resources.cert, "1qa@WS3ed", X509KeyStorageFlags.MachineKeySet);
+            return cert;
+        }
+    }
+}
